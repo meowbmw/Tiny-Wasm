f33ff53939f3b53d9f0562c6387f5e0d9bafabd9
meowbmw <wang.feng@partner.bmw.com>
2024-12-23 18:49:56 +0800

    support parse function section

--- parser.cpp
+++ parser.cpp
@@ -23,7 +23,7 @@ public:
     cout << "Decoding type section: " << s.substr(0, length * 2) << endl;
     const u_int64_t type_count = stoul(s.substr(0, 2), nullptr, 16);
     cout << "Total type count: " << type_count << endl;
-    uint64_t base_offset = 2; // Warn: skip 60(function type identifier) as it is fixed
+    uint64_t base_offset = 2; // Warn: skip one byte: 60 (function type identifier) as it is fixed
     for (int i = 0; i < type_count; ++i) {
       cout << "--- Info for type " << i << " ---" << endl;
       WasmType curType;
@@ -40,6 +40,20 @@ public:
       }
       curType.print_data(TypeCategory::RESULT);
       base_offset = base_offset + 2 + 2 * result_count;
+      wasmTypeVec.push_back(curType);
+    }
+  }
+  void parse_function() {
+    // function section
+    cout << "Decoding function section: " << s.substr(0, length * 2) << endl;
+    const u_int64_t function_count = stoul(s.substr(0, 2), nullptr, 16);
+    cout << "Total function count: " << function_count << endl;
+    uint64_t base_offset = 0;
+    for (int i = 0; i < function_count; ++i) {
+      cout << "--- Info for function " << i << " ---" << endl;
+      auto function_type = stoul(s.substr(base_offset + 2 * i + 2, 2), nullptr, 16);
+      funcTypeVec.push_back(function_type);
+      cout << "Type is: " << function_type << endl;
     }
   }
   void parse_export() {
@@ -87,30 +101,37 @@ public:
       base_offset = base_offset + 2 * func_size + 2;
       curFunc.set_code_vec(opcodes, local_var_declare_count);
       curFunc.processCodeVec();
+      wasmFunctionVec.push_back(curFunc);
     }
   }
 
 private:
-  const string &s;
-  const unsigned int &length;
+  const string &s;                      // should be read-only
+  const unsigned int &length;           // should be read-only
+  vector<WasmFunction> wasmFunctionVec; // used to store function code
+  vector<WasmType> wasmTypeVec;         // used to store type definition
+  vector<int> funcTypeVec;
 };
 
 int main() {
   // aarch64-linux-gnu-g++ -c arm64.s && aarch64-linux-gnu-objdump -d arm64.o
   // aarch64-linux-gnu-g++ parser.cpp -o parser && qemu-aarch64 -L /usr/aarch64-linux-gnu ./parser
   // qemu-aarch64 -g 1234 -L /usr/aarch64-linux-gnu ./parser
-  string wasm_to_read = "test/local.1.wasm";
+  string wasm_to_read = "test/local.2.wasm";
   cout << "Parsing wasm file: " << wasm_to_read << endl;
   string s = readBinary(wasm_to_read);
   initial_check(s);
   while (s.size() > 0) {
     const string type = s.substr(0, 2);
     s = s.substr(2);                                                // crop type
-    const unsigned int length = stoul(s.substr(0, 2), nullptr, 16); // Warn: we assume that the length is at max 1 byte!!!
+    const unsigned int length = stoul(s.substr(0, 2), nullptr, 16); // Warn & TODO: we assume that the length is at max 1 byte!!!
     s = s.substr(2);                                                // crop length
     Parser parser(s, length);
     if (type == "01") {
       parser.parse_type();
+    }
+    if (type == "03") {
+      parser.parse_function();
     }
     if (type == "07") {
       // parser.parse_export();
@@ -118,7 +139,7 @@ int main() {
     if (type == "0a") {
       parser.parse_code();
     }
-    s = s.substr(length * 2); // move forward, remeber we need to times 2 because we are processing 2 char at a time; 2 char = 1 byte
+    s = s.substr(length * 2); // move forward, remeber we need to times 2 because we are processing 2 char at a time; 2 char = 2 * 4 bits = 1 byte
     // cout << type << " " << length << endl;
   }
 }
