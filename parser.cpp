498338f5f1b9bc3ac18036f2c201a6b21d7f128d
meowbmw <wang.feng@partner.bmw.com>
2024-12-24 13:54:22 +0800

    further refine output; support function index to name mapping

--- parser.cpp
+++ parser.cpp
@@ -7,10 +7,11 @@ using json = nlohmann::json;
 // aarch64-linux-gnu-g++ -c arm64.s && aarch64-linux-gnu-objdump -d arm64.o
 // aarch64-linux-gnu-g++ parser.cpp -o parser && qemu-aarch64 -L /usr/aarch64-linux-gnu ./parser
 // qemu-aarch64 -g 1234 -L /usr/aarch64-linux-gnu ./parser
-bool debug_export_section = false;
-bool debug_function_section = false;
-bool debug_type_section = false;
-bool debug_code_section = false;
+const bool DEBUG_EXPORT_SECTION = false;
+const bool DEBUG_FUNCTION_SECTION = false;
+const bool DEBUG_TYPE_SECTION = false;
+const bool DEBUG_CODE_SECTION = false;
+const string WASM_TO_READ = "test/local.2.wasm";
 void initial_check(string &s) {
   // check magic number and version
   cout << "Full Binary: " << s << endl;
@@ -28,29 +29,25 @@ public:
   }
   void parse_type() {
     // type section
-    cout << "Decoding type section: " << s.substr(0, length * 2) << endl;
     const u_int64_t type_count = stoul(s.substr(0, 2), nullptr, 16);
-    cout << "Total type count: " << type_count << endl;
     uint64_t base_offset = 2; // Warn: skip one byte: 60 (function type identifier) as it is fixed
+    cout << "Decoding type section: " << s.substr(0, length * 2) << endl;
+    cout << "Total type count: " << type_count << endl;
     for (int i = 0; i < type_count; ++i) {
-      if (debug_type_section) {
-        cout << "--- Info for type " << i << " ---" << endl;
-      }
       WasmType curType;
       const uint64_t param_count = stoul(s.substr(base_offset + 2, 2), nullptr, 16);
       base_offset = base_offset + 2;
       for (int j = 0; j < param_count; ++j) {
         curType.add_param(s.substr(base_offset + 2 + 2 * j, 2));
       }
-      if (debug_type_section) {
-        curType.print_data(TypeCategory::PARAM);
-      }
       base_offset = base_offset + 2 + 2 * param_count;
       const uint64_t result_count = stoul(s.substr(base_offset, 2), nullptr, 16);
       for (int j = 0; j < result_count; ++j) {
         curType.add_result(s.substr(base_offset + 2 + 2 * j, 2));
       }
-      if (debug_type_section) {
+      if (DEBUG_TYPE_SECTION) {
+        cout << "--- Info for type " << i << " ---" << endl;
+        curType.print_data(TypeCategory::PARAM);
         curType.print_data(TypeCategory::RESULT);
       }
       base_offset = base_offset + 2 + 2 * result_count;
@@ -59,48 +56,54 @@ public:
   }
   void parse_function() {
     // function section
-    cout << "Decoding function section: " << s.substr(0, length * 2) << endl;
     const u_int64_t function_count = stoul(s.substr(0, 2), nullptr, 16);
-    cout << "Total function count: " << function_count << endl;
     uint64_t base_offset = 0;
+    cout << "Decoding function section: " << s.substr(0, length * 2) << endl;
+    cout << "Total function count: " << function_count << endl;
     for (int i = 0; i < function_count; ++i) {
-      // cout << "--- Info for function " << i << " ---" << endl;
       auto function_type = stoul(s.substr(base_offset + 2 * i + 2, 2), nullptr, 16);
       funcTypeVec.push_back(function_type);
-      // cout << "Type is: " << function_type << endl;
+      if (DEBUG_FUNCTION_SECTION) {
+        cout << "--- Info for function " << i << " ---" << endl;
+        cout << "Type is: " << function_type << endl;
+      }
     }
   }
   void parse_export() {
     // export section
-    cout << "Decoding export section: " << s.substr(0, length * 2) << endl;
     const u_int64_t export_count = stoul(s.substr(0, 2), nullptr, 16);
-    cout << "Total export count: " << export_count << endl;
     uint64_t base_offset = 0;
+    cout << "Decoding export section: " << s.substr(0, length * 2) << endl;
+    cout << "Total export count: " << export_count << endl;
     for (int i = 0; i < export_count; ++i) {
       const u_int64_t export_size = stoul(s.substr(base_offset + 2, 2), nullptr, 16);
-      cout << "--- Info for export " << i << " ---" << endl;
       const string export_name = hexToAscii(s.substr(base_offset + 4, export_size * 2));
       const u_int64_t export_type = stoul(s.substr(base_offset + 2 * export_size + 4, 2), nullptr, 16);
       const u_int64_t export_index = stoul(s.substr(base_offset + 2 * export_size + 6, 2), nullptr, 16);
-      cout << "Type: " << export_type << endl;
-      cout << "Index: " << export_index << endl;
-      cout << "Name: " << export_name << endl;
+      funcNameIndexMapper[export_name] = export_index;
+      funcIndexNameMapper[export_index] = export_name;
+      if (DEBUG_EXPORT_SECTION) {
+        cout << "--- Info for export " << i << " ---" << endl;
+        cout << "Type: " << export_type << endl;
+        cout << "Index: " << export_index << endl;
+        cout << "Name: " << export_name << endl;
+      }
       base_offset = base_offset + 2 * export_size + 6;
     }
   }
   void parse_code() {
     // code section
-    cout << "Decoding code section: " << s.substr(0, length * 2) << endl;
     const u_int64_t func_count = stoul(s.substr(0, 2), nullptr, 16);
-    cout << "Total function count: " << func_count << endl;
     uint64_t base_offset = 0;
+    cout << "Decoding code section: " << s.substr(0, length * 2) << endl;
+    cout << "Total function count: " << func_count << endl;
     for (int i = 0; i < func_count; ++i) {
       WasmFunction curFunc;
       const u_int64_t func_size = stoul(s.substr(base_offset + 2, 2), nullptr, 16);
       const u_int64_t local_var_declare_count =
           stoul(s.substr(base_offset + 4, 2), nullptr,
                 16); // Warn!!! One declare could imply multiple variables so this does not really equal to the real variable count!!!
-      if (debug_code_section) {
+      if (DEBUG_CODE_SECTION) {
         cout << "--- Info for function " << i << " ---" << endl;
         cout << "Func size: " << func_size << endl;
         cout << "Local variable declare count: " << local_var_declare_count << endl;
@@ -109,7 +112,7 @@ public:
       for (int i = 0; i < func_size - 1; ++i) {
         opcodes.push_back(s.substr(base_offset + 6 + i * 2, 2));
       }
-      if (debug_code_section) {
+      if (DEBUG_CODE_SECTION) {
         cout << "Opcode: ";
         for (auto &c : opcodes) {
           cout << c << " ";
@@ -127,12 +130,13 @@ public:
   vector<WasmFunction> wasmFunctionVec; // used to store function code
   vector<WasmType> wasmTypeVec;         // used to store type definition
   vector<int> funcTypeVec;
+  map<string, int> funcNameIndexMapper;
+  map<int, string> funcIndexNameMapper;
 };
 
 int main() {
-  string wasm_to_read = "test/local.2.wasm";
-  cout << "Parsing wasm file: " << wasm_to_read << endl;
-  string s = readBinary(wasm_to_read);
+  cout << "Parsing wasm file: " << WASM_TO_READ << endl;
+  string s = readBinary(WASM_TO_READ);
   initial_check(s);
   Parser parser;
   while (s.size() > 0) {
@@ -148,7 +152,7 @@ int main() {
       parser.parse_function();
     }
     if (type == "07") {
-      // parser.parse_export();
+      parser.parse_export();
     }
     if (type == "0a") {
       parser.parse_code();
@@ -160,15 +164,16 @@ int main() {
     parser.wasmFunctionVec[i].type = parser.funcTypeVec[i];
     parser.wasmFunctionVec[i].param_data = parser.wasmTypeVec[parser.funcTypeVec[i]].param_data;
     parser.wasmFunctionVec[i].result_data = parser.wasmTypeVec[parser.funcTypeVec[i]].result_data;
-    cout << "---Processing function: " << i << "---" << endl;
+    cout << "---Processing function " << i << ": " << parser.funcIndexNameMapper[i] << "---" << endl;
     parser.wasmFunctionVec[i].processCodeVec(); // this function will deal with local varaible initialization and machine code construction
     cout << "Total param count: " << parser.wasmFunctionVec[i].param_data.size() << endl;
-    cout << "Total local count: " << parser.wasmFunctionVec[i].local_data.size() << endl; // NOTE: only output local count after processCodeVec or it will be wrong number!
+    cout << "Total local count: " << parser.wasmFunctionVec[i].local_data.size()
+         << endl; // NOTE: only output local count after processCodeVec or it will be wrong number!
     cout << "Total result count: " << parser.wasmFunctionVec[i].result_data.size() << endl;
   }
   cout << "---Executing Functions---" << endl;
   for (int i = 0; i < parser.wasmFunctionVec.size(); ++i) {
-    cout << "Executing Function: " << i << endl;
+    cout << "Executing function " << i << ": " << parser.funcIndexNameMapper[i] << endl;
     parser.wasmFunctionVec[i].executeInstr();
   }
 }
